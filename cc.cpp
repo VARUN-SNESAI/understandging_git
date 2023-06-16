1e964a2408634005579fc4b276fa7f9f100be93a
VARUN <devvarun.code@gmail.com>
2023-06-17 02:22:02 +0530

    verson 3

--- cc.cpp
+++ cc.cpp
@@ -1,1 +1,48 @@
-varsion 1 
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+void clearScreen()
+{
+    // Function to clear the console screen
+    std::cout << "\033[2J\033[1;1H";
+}
+
+void delay(int milliseconds)
+{
+    // Function to introduce delay in milliseconds
+    std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+}
+
+int main()
+{
+    int delayTime = 100; // Delay between animation frames in milliseconds
+    std::string heart = 
+        "   ***     ***   \n"
+        "  *****   *****  \n"
+        " ******* ******* \n"
+        " *************** \n"
+        "  *************  \n"
+        "   ***********   \n"
+        "    *********    \n"
+        "     *******     \n"
+        "      *****      \n"
+        "       ***       \n"
+        "        *        \n";
+        
+    std::string message = "I love you";
+
+    // Clear the screen and display the heart initially
+    clearScreen();
+    std::cout << heart;
+
+    // Animate the message
+    for (char& c : message) {
+        std::cout << c;
+        std::cout.flush();
+        delay(delayTime);
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
--- code.java
+++ code.java
@@ -1,8 +1,53 @@
-public class code {
-    public static void name (){
-        
-    }
+import java.util.Scanner;
+
+
+public class code  {
     public static void main(String[] args) {
-        
+        String[] frames = {
+            "        ***      \n" +
+            "     *********   \n" +
+            "   ************* \n" +
+            "  ***************\n" +
+            "   ************* \n" +
+            "     *********   \n" +
+            "        ***      \n",
+            
+            "       *****     \n" +
+            "    *********    \n" +
+            "  *************  \n" +
+            " ****************\n" +
+            "  *************  \n" +
+            "    *********    \n" +
+            "       *****     \n",
+            
+            "        ***      \n" +
+            "     *********   \n" +
+            "   ************* \n" +
+            "  ***************\n" +
+            "   ************* \n" +
+            "     *********   \n" +
+            "        ***      \n"
+        };
+
+        for (int i = 0; i < frames.length; i++) {
+            clearScreen();
+            System.out.print(frames[i]);
+            delay(500);
+        }
+
+        System.out.println("Animation complete!");
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void delay(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
